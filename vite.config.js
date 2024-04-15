884f9e6431b7257392231c237a6082dfc7a0f2e8
Duraiz Haider <86344507+xCyanidex@users.noreply.github.com>
2024-04-16 03:01:46 +0500

    changes to json file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactSnapPlugin from 'vite-plugin-react-snap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory is set to 'dist'
-  },
+  plugins: [react(), reactSnapPlugin({
+    injectScript: true,
+  }),],
+  
 })
