ab4c2c93eca1e947fe223c56bbde0caa39bfc32c
Duraiz Haider <86344507+xCyanidex@users.noreply.github.com>
2024-05-09 14:46:23 +0500

    changes to form

--- src/Components/CTA/CTA.jsx
+++ src/Components/CTA/CTA.jsx
@@ -91,7 +91,15 @@ const CTA = () => {
                       />
                     </div>
                     <button
-                      type="submit"
+                    
+                      onClick={() => {
+                        if (!inputs.fname || !inputs.lname || !inputs.email) {
+                          alert("Please complete all the fields");
+                        }else{
+                           document.querySelector("form").submit();
+                        }
+                      }}
+                      type="button"
                       data-te-ripple-init=""
                       data-te-ripple-color="light"
                       className="max-w-xs   mb-6 inline-block w-full rounded bg-[#CBA664] px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
