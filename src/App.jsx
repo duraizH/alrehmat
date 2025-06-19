747c783be806ea7a64633ea73ff5e31b226013b9
Duraiz Haider <haiderduraiz@gmail.com>
2025-06-19 15:50:10 -0400

    updated image card

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,77 @@
-
-import Footer from "./Components/Footer/Footer";
-import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Pages/Home";
+import { useEffect } from "react"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
+import Footer from "./Components/Footer/Footer"
+import Navbar from "./Components/Navbar/Navbar"
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
-import DealersRegistration from "./Pages/DealersRegistration";
-import Projects from "./Pages/Projects";
-import Meriton from "./Pages/Meriton";
-import { BrowserRouter, Routes, Route,useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import DealersRegistration from "./Pages/DealersRegistration"
+import Home from "./Pages/Home"
+import Meriton from "./Pages/Meriton"
+import Projects from "./Pages/Projects"
 // import { ReactComponent as CompanyIcon } from "./assets/CompanySVG";
-import { WhatsAppWidget } from "react-whatsapp-widget";
-import "react-whatsapp-widget/dist/index.css";
-import { Helmet } from "react-helmet";
-import ReactGA from "react-ga4";
-import RoyalResidenciaPage from "./Pages/RoyalResidencia";
+import ReactGA from "react-ga4"
+import { Helmet } from "react-helmet"
+import { WhatsAppWidget } from "react-whatsapp-widget"
+import "react-whatsapp-widget/dist/index.css"
+import RoyalResidenciaPage from "./Pages/RoyalResidencia"
 
-const TRACKING_ID = "G-111B6N0MNE";
-ReactGA.initialize(TRACKING_ID);
-
+const TRACKING_ID = "G-111B6N0MNE"
+ReactGA.initialize(TRACKING_ID)
 
 function App() {
+	// useEffect(() => {
+	//   ReactGA.pageview(window.location.pathname + window.location.search);
+	// }, []);
 
-  // useEffect(() => {
-  //   ReactGA.pageview(window.location.pathname + window.location.search);
-  // }, []);
+	function ScrollToTop() {
+		const { pathname } = useLocation()
 
-  function ScrollToTop() {
-    const { pathname } = useLocation();
+		useEffect(() => {
+			window.scrollTo(0, 0)
+		}, [pathname])
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return null;
-  }
-  return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Al Rehmat Developers</title>
-        <meta
-          name="google-site-verification"
-          content="ZaamaPju4b_YQslADdx5QhirEtNsrkxE8IGpPmEEHpE"
-        />
-        <link rel="canonical" href="https://alrehmatdevelopers.com/" />
-      </Helmet>
-      <BrowserRouter>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/registration" element={<DealersRegistration />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/meriton" element={<Meriton />} />
-          <Route
-            path="/projects/royal-residencia"
-            element={<RoyalResidenciaPage />}
-          />
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-      <WhatsAppWidget
-        style={{ zIndex: "99999999999", position: "relative" }}
-        inputPlaceHolder={"Type your message"}
-        companyName={"Al-Rehmat"}
-        message={
-          "Welcome to Al-Rehmat Developers,\n Please type your query and our agent will respond to you soon.\n Thank you."
-        }
-        phoneNumber="+923010000762"
-      />
-    </>
-  );
+		return null
+	}
+	return (
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Al Rehmat Developers</title>
+				<meta
+					name="google-site-verification"
+					content="ZaamaPju4b_YQslADdx5QhirEtNsrkxE8IGpPmEEHpE"
+				/>
+				<link rel="canonical" href="https://alrehmatdevelopers.com/" />
+			</Helmet>
+			<BrowserRouter>
+				<Navbar />
+				<ScrollToTop />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/registration" element={<DealersRegistration />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/projects/meriton" element={<Meriton />} />
+					<Route
+						path="/projects/alrehmat-residencia"
+						element={<RoyalResidenciaPage />}
+					/>
+					<Route path="*" element={<Home />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+			<WhatsAppWidget
+				style={{ zIndex: "99999999999", position: "relative" }}
+				inputPlaceHolder={"Type your message"}
+				companyName={"Al-Rehmat"}
+				message={
+					"Welcome to Al-Rehmat Developers,\n Please type your query and our agent will respond to you soon.\n Thank you."
+				}
+				phoneNumber="+923010000762"
+			/>
+		</>
+	)
 }
 
 export default App
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,36 +1,36 @@
-import Card from "../Components/Card/Card.jsx";
-import meritonCover from "../assets/meritonCover.jpg";
-import meritonModel from "../assets/meritonModel.png";
-import royalCover from "../assets/royalbg3.png";
-import royalText from "../assets/royalbg3-Photoroom.png";
-import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet"
+import { Link } from "react-router-dom"
+import cardRoyal from "../../public/royal/royalImages/cardRoyal.png"
+import cardRoyalBg from "../../public/royal/royalImages/cardRoyalBg.png"
+import meritonCover from "../assets/meritonCover.jpg"
+import meritonModel from "../assets/meritonModel.png"
+import Card from "../Components/Card/Card.jsx"
 
 const Projects = () => {
-  //  useEffect(() => {
-  //    ReactGA.pageview(window.location.pathname + window.location.search);
-  //  }, []);
-  return (
-    <>
-      <Helmet>
-        <title>Projects Al-Rehmat</title>
-        <meta name="description" content="Projects Al Rehmat Developers" />
-      </Helmet>
-      <section className="my-20">
-        <div className=" mx-auto grid container grid-cols-2 max-w-[1000px] justify-items-center align-baseline ">
-          <div className=" col-span-1">
-            <Link to={`/projects/meriton`}>
-              <Card imgsrcModel={meritonModel} imgsrcCover={meritonCover} />
-            </Link>
-          </div>
-          <div className=" col-span-1">
-            <Link to={`/projects/royal-residencia`}>
-              <Card imgsrcModel={royalText} imgsrcCover={royalCover} />
-            </Link>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
-export default Projects;
+	//  useEffect(() => {
+	//    ReactGA.pageview(window.location.pathname + window.location.search);
+	//  }, []);
+	return (
+		<>
+			<Helmet>
+				<title>Projects Al-Rehmat</title>
+				<meta name="description" content="Projects Al Rehmat Developers" />
+			</Helmet>
+			<section className="my-20">
+				<div className=" mx-auto grid container grid-cols-2 max-w-[1000px] justify-items-center align-baseline ">
+					<div className=" col-span-1">
+						<Link to={`/projects/meriton`}>
+							<Card imgsrcModel={meritonModel} imgsrcCover={meritonCover} />
+						</Link>
+					</div>
+					<div className=" col-span-1">
+						<Link to={`/projects/alrehmat-residencia`}>
+							<Card imgsrcModel={cardRoyal} imgsrcCover={cardRoyalBg} />
+						</Link>
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
+export default Projects
